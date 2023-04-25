efffadc95d07fe3725743beecdb2495eca06c43d
menna <120627307+MennatAllahhxx@users.noreply.github.com>
2023-04-25 21:38:35 +0200

    updated

---
+++ _rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _rev - a fun
+ * @str: op
+ * Return: length
+ */
+int _rev(va_list x)
+{
+char *str = va_arg(x, char*);
+int length = 0;
+int i;
+if (str == NULL)
+{
+str = "(null)";
+}
+while (str[length] != '\0')
+{
+length++;
+}
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+return (length);
+}
