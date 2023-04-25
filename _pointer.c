3516c294cf39c2218f94e83d484d903f494c5afd
menna <120627307+MennatAllahhxx@users.noreply.github.com>
2023-04-25 22:00:08 +0200

    updated

---
+++ _pointer.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _pointer - a fun
+ * @x: op
+ * Return: sth
+ */
+int _pointer(va_list x)
+{
+char *str = "(nil)";
+void *ptr;	
+unsigned long int k;
+int j;
+int i;
+ptr = va_arg(x, void*);
+if (ptr == NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+return (i);
+}
+k = (unsigned long int)pointer;
+_putchar('0');
+_putchar('x');
+j = _hex_aux(k);
+return (k + 2);
+}
